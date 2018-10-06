888211fe31a0956f4d83b52f2205b6afc0e6d224
Al-un <40738601+Al-un@users.noreply.github.com>
2018-10-06 23:01:07 +0200

    Rails: adding versioning to API paths

--- learn_ruby/api/config/routes.rb
+++ learn_ruby/api/config/routes.rb
@@ -1,8 +1,16 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  # Versioning the whole API, starts with V1
+  scope 'v1' do
 
-  # Articles
-  resources :articles
-  resources :catalogs
+    # Articles
+    resources :articles
+    resources :catalogs do
+      resources :article_publications
+      resources :articles, only: [:index, :show]
+    end
+
+  end
 
 end
