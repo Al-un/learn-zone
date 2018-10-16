37a544e7d1a023b82594561e2a352f5098e27f85
Al-un <40738601+Al-un@users.noreply.github.com>
2018-10-16 15:58:06 +0200

    Rails/Fullstack: auth0 environment variable + local environment variables

--- rails/fullstack/config/application.rb
+++ rails/fullstack/config/application.rb
@@ -15,5 +15,18 @@ module LearnZone
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Custom environment files
+    if Rails.env.development? 
+      config.before_configuration do
+        env_file = File.join(Rails.root, 'config', 'local_env.yml')
+        if File.exists?(env_file)
+          YAML.load(File.open(env_file)).each do |key, value|
+            ENV[key.to_s] = value
+          end 
+        end
+      end
+    end
+
   end
 end
--- rails/fullstack/config/initializers/auth0.rb
+++ rails/fullstack/config/initializers/auth0.rb
@@ -1,12 +1,22 @@
 # frozen_string_literal: true
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+  # provider(
+  #   :auth0,
+  #   'ObQqZ8cA6qe17h936NJW4B6nfFWjYRhU',
+  #   'GMAMBBc5t_UgS3AnjS_ZIAuKvO6cctQowKGa8emyQT3P-JqnXwBjn3B6Kr2yQq3F',
+  #   'al-un-dev.eu.auth0.com',
+  #   callback_path: '/auth/oauth2/callback',
+  #   authorize_params: {
+  #     scope: 'openid profile'
+  #   }
+  # )
   provider(
     :auth0,
-    'ObQqZ8cA6qe17h936NJW4B6nfFWjYRhU',
-    'GMAMBBc5t_UgS3AnjS_ZIAuKvO6cctQowKGa8emyQT3P-JqnXwBjn3B6Kr2yQq3F',
-    'al-un-dev.eu.auth0.com',
-    callback_path: '/auth/oauth2/callback',
+    ENV['AUTH0_CLIENT_ID'],
+    ENV['AUTH0_CLIENT_SECRET'],
+    ENV['AUTH0_DOMAIN'],
+    callback_path: ENV['AUTH0_CALLBACK'],
     authorize_params: {
       scope: 'openid profile'
     }
